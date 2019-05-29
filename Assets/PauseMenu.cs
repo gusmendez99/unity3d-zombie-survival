1b4472b6c1e2541f851bedfb0b1589a19032a06f
michelebenveuto <35434145+michelebenveuto@users.noreply.github.com>
2019-05-29 17:26:33 -0600

    main/pause menus

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class PauseMenu : MonoBehaviour
+{
+    private bool isPaused = false;
+    public Canvas UI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        UI.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            if (isPaused)
+            {
+                Continue();
+            }
+            else if (!isPaused)
+            {
+                Pause();
+            }
+        }
+    }
+    public void Pause()
+    {
+        Time.timeScale = 0.0f;
+        UI.enabled = true;
+    }
+    public void Continue()
+    {
+        Time.timeScale = 1.0f;
+        UI.enabled = false;
+    }
+
+    public void Return()
+    {
+        Application.Quit();
+    }
+}
